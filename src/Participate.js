8cd21548dfb48e8a54055026e06120e9e076bfa7
“Mohsin <mohsinrashid32@gmail.com  git config --global user.name Mohsin Rashidgit config --global user.email mohsinrashid32@gmail.com>
2021-07-25 16:12:51 +0500

    2nd and 3rd page done

--- src/Participate.js
+++ src/Participate.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import './css/App.css'
 import { Container, Button, Grid} from 'semantic-ui-react';
 import icon0 from './Images/i0.svg';
 import icon1 from './Images/i1.svg';
 import icon2 from './Images/i2.svg';
 import icon3 from './Images/i3.svg';
+import Slider from "react-slick";
+
 
 export default function Participate () {
-    
+    var settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
     return (
         <div className="participate">
             <Container>
@@ -37,19 +46,57 @@ export default function Participate () {
                                 </div>
                             </Grid.Column>
                             <Grid.Column className="right-section" width={10}>
-                                <h2>On this site</h2>
-                                <ol>
-                                    <li>Download the Polkadot Browser extension for Chrome/Firefox <a href="#">here</a>.</li>
-                                    <li>Ensure that you have KSM in your Polkadot.js account.</li>
-                                    <li>Unbond your KSM. See further details <a href="#">here</a>.</li>
-                                </ol>
-                                <div className="form mb-5">
-                                    <div className="boxes">
-                                        <label>KSM to Lock Up:</label>
-                                        <input text="tel" placeholder="Enter KSM Amount" />
+                                <Slider {...settings}>
+                                    <div>
+                                        <h2>On this site</h2>
+                                        <ol>
+                                            <li>Download the Polkadot Browser extension for Chrome/Firefox <a href="#">here</a>.</li>
+                                            <li>Ensure that you have KSM in your Polkadot.js account.</li>
+                                            <li>Unbond your KSM. See further details <a href="#">here</a>.</li>
+                                        </ol>
+                                        <div className="form mb-5">
+                                            <div className="boxes">
+                                                <label>KSM to Lock Up:</label>
+                                                <input text="tel" placeholder="Enter KSM Amount" />
+                                            </div>
+                                        </div>
+                                        <Button className="gradient-btn">Participate Now</Button>
                                     </div>
-                                </div>
-                                <Button className="gradient-btn">Participate Now</Button>
+                                    <div>
+                                        <h2>Through an Exchange</h2>
+                                        <p>If you hold KSM on a crypto exchange, it may provide functionality to participate in Kusama crowdloans from your user account. </p>
+                                        <div className="click">Click on one of the exchanges below for more details:</div>
+                                        <div className="image-holder">
+                                            <div className="images">
+                                                <img src={icon1} />
+                                            </div>
+                                            <div className="images">
+                                                <img src={icon2} />
+                                            </div>
+                                            <div className="images">
+                                                <img src={icon3} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h2>Using Polkadot-JS Apps</h2>
+                                        <ol>
+                                            <li>Navigate to: <a href="#">polkadot.js.org/apps/</a>.</li>
+                                            <li>Follow the instructions detailed in this video:</li>
+                                        </ol>
+                                        <div className="video-box">
+                                            <div className="video-placeholder">
+                                                <iframe
+                                                src="https://www.youtube.com/embed/nWwpyclIEu4"
+                                                frameBorder="0"
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                allowFullScreen
+                                                title="Embedded youtube"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Slider>
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
